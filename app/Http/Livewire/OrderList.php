d72a59d06527e2cf3127c922ab649f8d06136375
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-13 21:26:03 -0400

    RefundComponents boilerplate

---
+++ app/Http/Livewire/OrderList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class OrderList extends Component
+{
+    public function render()
+    {
+        return view('livewire.order-list');
+    }
+}
---
+++ app/Http/Livewire/OrderListItemCounter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class OrderListItemCounter extends Component
+{
+    public function render()
+    {
+        return view('livewire.order-list-item-counter');
+    }
+}
