74bf785443535235746e793d5ee6d1516c3a0eae
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-15 13:23:31 -0400

    Separate shopify login logic to it's own file

---
+++ resources/views/includes/shopify-scaffolding.blade.php
@@ -0,0 +1,19 @@
+@if(\Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_enabled'))
+    <script src="https://unpkg.com/@shopify/app-bridge{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
+    <script src="https://unpkg.com/@shopify/app-bridge-utils{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
+    <script data-turbolinks-eval="false" >
+        var AppBridge = window['app-bridge'];
+        var actions = AppBridge.actions;
+        var utils = window['app-bridge-utils'];
+        var createApp = AppBridge.default;
+        var app = createApp({
+            apiKey: "{{ \Osiset\ShopifyApp\Util::getShopifyConfig('api_key', $shopDomain ?? Auth::user()->name ) }}",
+            shopOrigin: "{{ $shopDomain ?? Auth::user()->name }}",
+            host: "{{ \Request::get('host') }}",
+            forceRedirect: true,
+        });
+    </script>
+
+    @include('shopify-app::partials.token_handler')
+    @include('shopify-app::partials.flash_messages')
+@endif
