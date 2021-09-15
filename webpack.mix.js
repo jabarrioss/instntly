7324dd7c75164bce1ca8b410fcced8d1655c3f69
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-15 13:37:00 -0400

    Dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
+const { copyDirectory } = require('laravel-mix');
 const mix = require('laravel-mix');
 
 /*
@@ -12,6 +13,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    copyDirectory('resources/assets', 'public/black')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
