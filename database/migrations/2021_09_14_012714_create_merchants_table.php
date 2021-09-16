a74ec1faf6b581b43c2e2b079af119e1dba8943c
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-16 07:40:36 -0400

    Fixed migrations

--- database/migrations/2021_09_14_012714_create_merchants_table.php
+++ database/migrations/2021_09_14_012714_create_merchants_table.php
@@ -16,8 +16,8 @@ class CreateMerchantsTable extends Migration
         Schema::create('merchants', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
-            $table->string('access_token')->nullable();
-            $table->string('refresh_token')->nullable();
+            $table->text('access_token')->nullable();
+            $table->text('refresh_token');
             $table->timestamps();
         });
     }
