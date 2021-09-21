eb103a6b6a583dbcdfabf2d82068df77fcca07db
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-20 20:37:41 -0400

    Added documentation to the AwsClient Class

--- app/Clients/AwsClient.php
+++ app/Clients/AwsClient.php
@@ -4,8 +4,25 @@ namespace App\Clients;
 use Ellaisys\Cognito\AwsCognitoClient as BaseCognitoClient;
 use Aws\CognitoIdentityProvider\Exception\CognitoIdentityProviderException;
 
+/**
+ * Class that extends from the base client of the cognito sdk to be able to obtain a new access_token from the AWS service
+ * 
+ * Created date: 20/09/2021
+ * Modified date: 20/09/2021
+ *
+ * Developed by: Jhonny B. Sandrea for Kleverpay
+ */
 class AwsClient extends BaseCognitoClient
 {
+    /**
+     * This method returns a new access_token using
+     * the refresh token provided through the API exposed to the client
+     * @param  \string  $username
+     * @param  \string  $refresh_token
+     * 
+     * @return \Aws\Result
+     */
+    
     public function getNewAccessTokenByRefreshToken($username, $refresh_token)
     {
         try {
