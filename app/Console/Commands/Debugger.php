337ee8940424028883227692279edb80c8ff7271
Jhonny Armando Barrios Sandrea <jbarriossandrea@gmail.com>
2021-09-27 19:55:11 -0400

    Added debugger command

---
+++ app/Console/Commands/Debugger.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Clients\AwsClient;
+use App\Models\Merchant;
+
+class Debugger extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'debug:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command for testing things';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $merchant = Merchant::find(2);
+        $awsClient =  app()->make(AwsClient::class);
+        $response = $awsClient->getNewAccessTokenByRefreshToken($merchant->username, $merchant->refresh_token);
+        $responseData = $response->get("AuthenticationResult");
+        $token = $responseData["AccessToken"];
+        dump($response);
+        $this->info($token);
+        return 0;
+    }
+}
