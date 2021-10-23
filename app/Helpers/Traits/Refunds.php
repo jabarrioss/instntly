907f5ee8af1f66405acdde72fb7e7ddeee3afa4f
Jhonny B <jbarriossandrea@gmail.com>
2021-10-23 17:14:24 -0400

    Refund refix

--- app/Helpers/Traits/Refunds.php
+++ app/Helpers/Traits/Refunds.php
@@ -43,6 +43,6 @@ trait Refunds
             return ["error" => false, "fundOrderId" => $response->collect()['fundOrder']['fundOrderId']];
         }
 
-        return ["error" => true, "message" => $response->json()];
+        return ["error" => true, "message" => $response->body()];
     }
 }
